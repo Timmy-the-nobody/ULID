b607c13ba6a4ae8cb021e623fda80ed72c5fb51e
Timmy <timmy.the.nobody@gmail.com>
2024-05-09 16:39:59 +0200

    style: add license headers

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -1,1 +1,7 @@
+--[[
+    ULID
+    GNU General Public License v3.0
+    Copyright © Timmy-the-nobody, 2024, https://github.com/Timmy-the-nobody
+]]--
+
 Package.Require("Shared/ulid.lua")
--- Shared/ulid.lua
+++ Shared/ulid.lua
@@ -1,3 +1,9 @@
+--[[
+    ULID
+    GNU General Public License v3.0
+    Copyright © Timmy-the-nobody, 2024, https://github.com/Timmy-the-nobody
+]]--
+
 local rand = math.random
 local concat = table.concat
 local format = string.format
